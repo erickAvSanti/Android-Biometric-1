93af4e8b6ed94a7382427d5d420e9f3607d9da1a
Erick Avalos S <eavalos@claroinsurance.com>
2021-09-19 02:09:52 -0500

    nice codding

--- app/src/main/java/com/karlituxd/androidbiometric1/MainActivity.kt
+++ app/src/main/java/com/karlituxd/androidbiometric1/MainActivity.kt
@@ -10,7 +10,6 @@ import android.widget.Button
 import android.widget.Toast
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.annotation.RequiresApi
 import androidx.biometric.BiometricManager
 import androidx.biometric.BiometricManager.Authenticators.*
 import androidx.biometric.BiometricPrompt
